6c327e08e84f9ac6130dbaa3fe1acf11b8c828d6
Steven De Ryck <steven@inform.be>
2020-01-16 13:59:10 +0100

    Klassikaal verbeterd

--- 1.1Formulier/js/index.js
+++ 1.1Formulier/js/index.js
@@ -1,24 +1,35 @@
-function myFunction(){
+function CheckNames( el_name )
+{
+  var lengte = document.getElementById( el_name ).value.length;
+
+    if ( lengte<2 ) {
+        var bevesting= document.getElementById("bevestiging");
+        bevesting.innerHTML="Element " + el_name + " is te kort"
+        bevesting.classList.add("red");
+    }
+}
+
+function Validatie(){
     
   var naam= document.getElementById('per_voornaam').value;
   var voornaam= document.getElementById('per_naam').value;
   var inschrijvingen= document.getElementById('inschrijvingen').value;
   
   inschrijvingen= parseFloat(inschrijvingen)+1;
-  var button= document.getElementById("bevestiging");
-  
-  if (naam.length<3||voornaam.length<3) {
-    button.innerHTML="Uw naam/voornaam is te kort"
-    button.classList.add("red");
+  var bevesting= document.getElementById("bevestiging");
+
+  if ( naam.length < 3 || voornaam.length < 3 ) {
+    bevesting.innerHTML="Uw naam/voornaam is te kort"
+    bevesting.classList.add("red");
   }
-  else if (inschrijvingen === 'NaN') {
-    button.innerHTML="Gelieve het aantal deelnemers juist in te vullen!"
-    button.classList.add("red");
+  else if ( ! ( Number.isInteger(inschrijvingen) ) ) {
+    bevesting.innerHTML="Het aantal is geen cijfer"
+    bevesting.classList.add("red");
   }
   else {
-    button.innerHTML="U schreef zich in voor "+inschrijvingen+" personen!";
-    button.classList.add("green");
-    button.classList.remove("red");
+    bevesting.innerHTML="U schreef zich in voor "+inschrijvingen+" personen!";
+    bevesting.classList.add("green");
+    bevesting.classList.remove("red");
   }
   
 }
--- 1.2Slideshow/js/index.js
+++ 1.2Slideshow/js/index.js
@@ -1,21 +1,39 @@
 images=["product_01.jpeg","product_02.jpeg","product_03.jpeg","product_04.jpeg","product_05.jpeg"];
-image= 0;
-document.getElementById("slide").style.background= "url('images/" + images[image] + "')";
+image = 0;
+var slide = document.getElementById("slide");
+var fout = document.getElementById("fout");
+var gekozen = document.getElementById("gekozen");
+
+PlaatsFoto();
+
+function PlaatsFoto()
+{
+    slide.style.background= "url('images/" + images[image] + "')";
+}
+
+function ToonFout( foutmelding )
+{
+    fout.innerHTML = foutmelding;
+}
 
 function volgendeFoto(){
-  if (image<4) {
-    image++;
-  }
-  else{image=0}
-  document.getElementById("slide").style.background= "url('images/" + images[image] + "')";
-  document.getElementById("fout").innerHTML= "";
+    //de volgende foto tonen
+  if ( image < 4 ) image++; //bij foto 4 weer naar 0 gaan
+  else image=0;
+
+    RefreshFoto()
 }
 
 function kiesFoto(){
-  image = document.getElementById("gekozen").value - 1;
-  if (image<5) {
-    document.getElementById("slide").style.background= "url('images/" + images[image] + "')";
-    document.getElementById("fout").innerHTML= "";
-  }
-  else{document.getElementById("fout").innerHTML= "Er zijn maar 5 afbeeldingen"}
+    //een met een cijfer gekozen foto tonen
+    image = gekozen.value - 1;
+
+    if ( ! ( image >= 0 && image < 5 ) ) ToonFout("Er zijn maar 5 afbeeldingen");
+    else RefreshFoto();
+}
+
+function RefreshFoto()
+{
+    PlaatsFoto();       //afbeelding laden
+    ToonFout("");     //leegmaken fouten
 }
